50546cdf1708c45cf1e26a6dd080fc725f46863f
Arman Yaraee <arman.yaraee@deversifi.com>
2023-06-02 09:57:23 +0100

    Pin nix dependendencies to our rhino-core version

--- shell.nix
+++ shell.nix
@@ -1,4 +1,12 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ 
+  pkgs ? (let 
+    nixpkgs = builtins.fetchTarball({
+      url = "https://github.com/nixos/nixpkgs/archive/8e3fad82be64c06fbfb9fd43993aec9ef4623936.tar.gz";
+      sha256 = "0x4yzsrrzq2q1ah77yydjg35niirx7yxvpzdryfnw0cr5bkszd3d";
+    }); in
+      import nixpkgs {}
+  )
+}:
 
 pkgs.mkShell {
   name = "backend-test";
